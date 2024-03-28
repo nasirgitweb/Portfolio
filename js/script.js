1acab406460dea467b9d8b4460cdabe84fdd4ead
nasirgitweb <freelancernasir46@gmail.com>
2024-03-28 07:45:25 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// var typed = new Typed(".typing",{
+//     Strings:["","Web Developer","Software Engineer","Digital Marketer","Freelancer","Social Media Manager"],
+//     typeSpeed:50,
+//     BackSpeed:10,
+//     loop:true
+// })
